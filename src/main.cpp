b8a2761ee515f339faf6f12b83fc4d04f2a08488
LorisMain <loris.bovcon07@gmail.com>
2026-01-02 17:33:46 +0100

    a bit of work on read/write and also added structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  printf("halloooo!!!\n");
+  char in[8] = { 0 };
+  do{
+    scanf("%s", &in[0]);
+    printf("hallo, %s", in);
+  } while(in[0] != 'q');
   return 0;
 }
